8715d16a4ea836474d75b316687848e1526d2ab7
Bradley Blackwell <blackwell.bh@gmail.com>
2023-05-17 10:21:38 -0400

    Message on HTML updated

--- FunctionApp/Get-AZRunningVM/run.ps1
+++ FunctionApp/Get-AZRunningVM/run.ps1
@@ -24,7 +24,7 @@ TD {border-width: 1px; padding: 3px; border-style: solid; border-color: black; p
     $RunningVmsHTML = $RunningVMs | ConvertTo-Html -property "ResourceGroupName", "Name", "OsType", "PowerState"
 
     # Combine HTML elements for output
-    $Header + "The Following VMs are not running <p>" + $NotRunningVmsHTML + "The Following VMs are currently running <p>" + $RunningVmsHTML
+    $Header + "<b>ALL VM'S IN THIS SUBSCRIPTION AUTO SHUT DOWN AT 7 PM AND START UP AT 6 AM EST DAILY!</b> <p> The Following VMs are not running <p>" + $NotRunningVmsHTML + "<p> To request a VM be turned on, email the name of the VM in the subject line to <a href=`"mailto:support_afcent_cloud_dl@afcentcloud.mil?Subject=Please%20turn%20on%20VM%20`">Support</a>. <p>" + "The Following VMs are currently running <p>" + $RunningVmsHTML
 
 }
 $HTML = Get-RunningVmHTML
